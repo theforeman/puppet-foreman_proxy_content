0e3108a06a7346eaec9799bbd498805f36e986bd
Stephen Benjamin <stephen@bitbin.de>
2017-05-15 10:46:03 -0400

    fixes #19269 - allow browsing /pub over https (#119)
    (cherry picked from commit 96b2e8db5746ad6129004bbb35b535e23532f60e)

--- manifests/init.pp
+++ manifests/init.pp
@@ -227,7 +227,7 @@ class foreman_proxy_content (
     }
 
     pulp::apache::fragment{'gpg_key_proxy':
-      ssl_content => template('foreman_proxy_content/_pulp_gpg_proxy.erb'),
+      ssl_content => template('foreman_proxy_content/_pulp_gpg_proxy.erb', 'foreman_proxy_content/httpd_pub.erb'),
     }
   }
 
