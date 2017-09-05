2cfba42ae5297a4a7a1b58804213ec5dc867c184
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-09-05 13:41:35 +0200

    Use puppetlabs-apache 2.0.0 parameters
    There are now parameters so we don't need a custom fragment anymore.

--- manifests/reverse_proxy.pp
+++ manifests/reverse_proxy.pp
@@ -11,28 +11,29 @@ class foreman_proxy_content::reverse_proxy (
   Class['certs', 'certs::ca', 'certs::apache', 'certs::foreman_proxy'] ~> Class['apache::service']
 
   apache::vhost { 'katello-reverse-proxy':
-    servername        => $::foreman_proxy_content::foreman_proxy_fqdn,
-    port              => $port,
-    docroot           => '/var/www/',
-    priority          => '28',
-    ssl_options       => ['+StdEnvVars', '+ExportCertData', '+FakeBasicAuth'],
-    ssl               => true,
-    ssl_proxyengine   => true,
-    ssl_cert          => $::certs::apache::apache_cert,
-    ssl_key           => $::certs::apache::apache_key,
-    ssl_ca            => $::certs::ca_cert,
-    ssl_certs_dir     => '',
-    ssl_verify_client => 'optional',
-    ssl_verify_depth  => 10,
-    request_headers   => ['set X_RHSM_SSL_CLIENT_CERT "%{SSL_CLIENT_CERT}s"'],
-    proxy_pass        => [
+    servername             => $::foreman_proxy_content::foreman_proxy_fqdn,
+    port                   => $port,
+    docroot                => '/var/www/',
+    priority               => '28',
+    ssl_options            => ['+StdEnvVars', '+ExportCertData', '+FakeBasicAuth'],
+    ssl                    => true,
+    ssl_proxyengine        => true,
+    ssl_proxy_ca_cert      => $::certs::ca_cert,
+    ssl_proxy_machine_cert => $::certs::foreman_proxy::foreman_proxy_ssl_client_bundle,
+    ssl_cert               => $::certs::apache::apache_cert,
+    ssl_key                => $::certs::apache::apache_key,
+    ssl_ca                 => $::certs::ca_cert,
+    ssl_verify_client      => 'optional',
+    ssl_verify_depth       => 10,
+    request_headers        => ['set X_RHSM_SSL_CLIENT_CERT "%{SSL_CLIENT_CERT}s"'],
+    proxy_pass             => [
       {
         'path'         => $path,
         'url'          => $url,
         'reverse_urls' => [$path, $url]
       }
     ],
-    error_documents   => [
+    error_documents        => [
       {
         'error_code' => '500',
         'document'   => '\'{"displayMessage": "Internal error, contact administrator", "errors": ["Internal error, contact administrator"], "status": "500" }\''
@@ -42,9 +43,5 @@ class foreman_proxy_content::reverse_proxy (
         'document'   => '\'{"displayMessage": "Service unavailable or restarting, try later", "errors": ["Service unavailable or restarting, try later"], "status": "503" }\''
       },
     ],
-    custom_fragment   => "
-      SSLProxyCACertificateFile ${::certs::ca_cert}
-      SSLProxyMachineCertificateFile ${::certs::foreman_proxy::foreman_proxy_ssl_client_bundle}
-    ",
   }
 }
