dcdd18b03b475040a0b912e56f2fe0dd7fb7dff5
Timo Goebel <mail@timogoebel.name>
2017-10-08 21:35:00 +0200

    extract pub dir setup

---
+++ manifests/pub_dir.pp
@@ -0,0 +1,21 @@
+# Manages the pub httpd directory where
+# e.g. the katello ca certificate is available
+# for download
+class foreman_proxy_content::pub_dir (
+  String $servername = $::fqdn
+) {
+  include ::foreman_proxy_content
+  include ::apache
+
+  ensure_packages('katello-client-bootstrap')
+
+  apache::vhost { 'foreman_proxy_content':
+    servername          => $servername,
+    port                => 80,
+    priority            => '05',
+    docroot             => '/var/www/html',
+    options             => ['SymLinksIfOwnerMatch'],
+    additional_includes => ['/etc/pulp/vhosts80/*.conf'],
+    custom_fragment     => template('foreman_proxy_content/httpd_pub.erb'),
+  }
+}
--- spec/classes/foreman_proxy_content_spec.rb
+++ spec/classes/foreman_proxy_content_spec.rb
@@ -11,7 +11,6 @@ describe 'foreman_proxy_content' do
 
 
       it { should contain_package('katello-debug') }
-      it { should contain_package('katello-client-bootstrap') }
 
       context 'with pulp' do
         let(:params) do
@@ -28,6 +27,8 @@ describe 'foreman_proxy_content' do
 
         it { should contain_class('pulp').with(:oauth_secret => 'mysecret') }
         it { should_not contain_class('foreman_proxy_content::dispatch_router') }
+
+        it { should contain_class('foreman_proxy_content::pub_dir') }
 
         it { should contain_pulp__apache__fragment('gpg_key_proxy').with({
           :ssl_content => %r{ProxyPass /katello/api/repositories/}} ) }
