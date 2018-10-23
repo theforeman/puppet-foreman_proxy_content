06b642fc3a5ed980609c22e7040e9843aa1a1ea2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-10-23 18:15:04 +0200

    Update styling of foreman_proxy_content_spec.rb

--- spec/classes/foreman_proxy_content_spec.rb
+++ spec/classes/foreman_proxy_content_spec.rb
@@ -5,47 +5,52 @@ describe 'foreman_proxy_content' do
     context "on #{os}" do
       let(:facts) { facts }
 
-      let :pre_condition do
-        "include foreman_proxy"
-	  end
-
-
-      it { should contain_package('katello-debug') }
+      context 'without parameters' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_package('katello-debug') }
+      end
 
       context 'with pulp' do
         let(:params) do
           {
-            :qpid_router       => false
+            qpid_router: false
           }
         end
 
         let(:pre_condition) do
-          "include foreman_proxy
-          class {'foreman_proxy::plugin::pulp': pulpnode_enabled => true}"
+          <<-PUPPET
+          include foreman_proxy
+          class { 'foreman_proxy::plugin::pulp':
+            pulpnode_enabled => true,
+          }
+          PUPPET
         end
 
-        it { should contain_class('pulp').with(:manage_squid => true) }
-        it { should_not contain_class('foreman_proxy_content::dispatch_router') }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('pulp').with(manage_squid: true) }
+        it { is_expected.not_to contain_class('foreman_proxy_content::dispatch_router') }
 
-        it { should contain_class('foreman_proxy_content::pub_dir') }
+        it { is_expected.to contain_class('foreman_proxy_content::pub_dir') }
 
-        it { should contain_pulp__apache__fragment('gpg_key_proxy').with({
-          :ssl_content => %r{ProxyPass /katello/api/repositories/ https://foo\.example\.com/katello/api/repositories/}} ) }
+        it do
+          is_expected.to contain_pulp__apache__fragment('gpg_key_proxy')
+            .with_ssl_content(%r{ProxyPass /katello/api/repositories/ https://foo\.example\.com/katello/api/repositories/})
+        end
       end
 
       context 'with rhsm_hostname and rhsm_url' do
         let(:params) do
           {
-            :rhsm_hostname => 'katello.example.com',
-            :rhsm_url      => '/abc/rhsm'
+            rhsm_hostname: 'katello.example.com',
+            rhsm_url: '/abc/rhsm'
           }
+        end
 
-          it do
-            should contain_class('certs::katello').with(
-              :hostname => 'katello.example.com',
-              :deployment_url => '/abc/rhsm'
-            )
-          end
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_class('certs::katello')
+            .with_hostname('katello.example.com')
+            .with_deployment_url('/abc/rhsm')
         end
       end
     end
