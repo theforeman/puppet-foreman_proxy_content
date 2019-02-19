e7e193234d9562238e01230d1d89fe4cf828e410
Ivan Nečas <inecas@redhat.com>
2019-02-19 17:46:00 +0100

    Refs #24974 - fix lint issues

--- manifests/reverse_proxy.pp
+++ manifests/reverse_proxy.pp
@@ -12,30 +12,30 @@ class foreman_proxy_content::reverse_proxy (
   Class['certs', 'certs::ca', 'certs::apache', 'certs::foreman_proxy'] ~> Class['apache::service']
 
   apache::vhost { 'katello-reverse-proxy':
-    servername             => $foreman_proxy_content::foreman_proxy_fqdn,
-    port                   => $port,
-    docroot                => '/var/www/',
-    priority               => '28',
-    ssl_options            => ['+StdEnvVars', '+ExportCertData', '+FakeBasicAuth'],
-    ssl                    => true,
-    ssl_proxyengine        => true,
-    ssl_proxy_ca_cert      => $certs::ca_cert,
-    ssl_cert               => $certs::apache::apache_cert,
-    ssl_key                => $certs::apache::apache_key,
-    ssl_chain              => $certs::katello_server_ca_cert,
-    ssl_ca                 => $certs::ca_cert,
-    ssl_verify_client      => 'optional',
-    ssl_verify_depth       => 10,
-    ssl_protocol           => $ssl_protocol,
-    request_headers        => ['set X_RHSM_SSL_CLIENT_CERT "%{SSL_CLIENT_CERT}s"'],
-    proxy_pass             => [
+    servername        => $foreman_proxy_content::foreman_proxy_fqdn,
+    port              => $port,
+    docroot           => '/var/www/',
+    priority          => '28',
+    ssl_options       => ['+StdEnvVars', '+ExportCertData', '+FakeBasicAuth'],
+    ssl               => true,
+    ssl_proxyengine   => true,
+    ssl_proxy_ca_cert => $certs::ca_cert,
+    ssl_cert          => $certs::apache::apache_cert,
+    ssl_key           => $certs::apache::apache_key,
+    ssl_chain         => $certs::katello_server_ca_cert,
+    ssl_ca            => $certs::ca_cert,
+    ssl_verify_client => 'optional',
+    ssl_verify_depth  => 10,
+    ssl_protocol      => $ssl_protocol,
+    request_headers   => ['set X_RHSM_SSL_CLIENT_CERT "%{SSL_CLIENT_CERT}s"'],
+    proxy_pass        => [
       {
         'path'         => $path,
         'url'          => $url,
         'reverse_urls' => [$path, $url]
       }
     ],
-    error_documents        => [
+    error_documents   => [
       {
         'error_code' => '500',
         'document'   => '\'{"displayMessage": "Internal error, contact administrator", "errors": ["Internal error, contact administrator"], "status": "500" }\''
