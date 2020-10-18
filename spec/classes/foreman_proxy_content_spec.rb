00992c7dbed2e8b494af56b50aac08078669a977
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-10-18 14:57:56 +0200

    Use RSpec's context nesting
    With RSpec contexts can be nested and they'll inherit the parent let
    blocks. The descriptions will be included as well.

--- spec/classes/foreman_proxy_content_spec.rb
+++ spec/classes/foreman_proxy_content_spec.rb
@@ -85,148 +85,97 @@ describe 'foreman_proxy_content' do
             .with(apache_https_vhost: 'foreman-ssl')
             .that_comes_before('Class[foreman_proxy::plugin::pulp]')
         end
-      end
 
-      context 'with pulpcore mirror' do
-        let(:params) do
-          {
-            qpid_router: false
-          }
+        context 'as mirror' do
+          let(:pre_condition) do
+            <<-PUPPET
+            include foreman_proxy
+            class { 'foreman_proxy::plugin::pulp':
+              enabled          => false,
+              pulpnode_enabled => false,
+              pulpcore_enabled => true,
+              pulpcore_mirror  => true,
+            }
+            PUPPET
+          end
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('foreman_proxy_content::pub_dir') }
+          it do
+            is_expected.to contain_class('pulpcore')
+              .with(apache_http_vhost: true)
+              .with(apache_https_vhost: true)
+              .that_comes_before('Class[foreman_proxy::plugin::pulp]')
+          end
+          it do
+            is_expected.to contain_class('foreman_proxy_content::reverse_proxy')
+              .with(path: '/')
+              .with(port: 8443)
+          end
         end
 
-        let(:pre_condition) do
-          <<-PUPPET
-          include foreman_proxy
-          class { 'foreman_proxy::plugin::pulp':
-            enabled          => false,
-            pulpnode_enabled => false,
-            pulpcore_enabled => true,
-            pulpcore_mirror  => true,
-          }
-          PUPPET
+        context 'with external postgres' do
+          let(:params) do
+            super().merge(
+              pulpcore_manage_postgresql: false,
+              pulpcore_postgresql_host: 'postgres-pulpcore.example.com',
+              pulpcore_postgresql_port: 2345,
+              pulpcore_postgresql_user: 'pulpuser',
+              pulpcore_postgresql_password: 'sUpersEkret',
+              pulpcore_postgresql_db_name: 'pulpcore1'
+            )
+          end
+
+          it { is_expected.to compile.with_all_deps }
+
+          it do
+            is_expected.to contain_class('pulpcore')
+              .with_postgresql_manage_db(false)
+              .with_postgresql_db_host('postgres-pulpcore.example.com')
+              .with_postgresql_db_port(2345)
+              .with_postgresql_db_user('pulpuser')
+              .with_postgresql_db_password('sUpersEkret')
+              .with_postgresql_db_name('pulpcore1')
+              .with_postgresql_db_ssl(false)
+          end
+
+          context 'and SSL' do
+            let(:params) do
+              super().merge(
+                pulpcore_postgresql_ssl: true,
+                pulpcore_postgresql_ssl_require: true,
+                pulpcore_postgresql_ssl_cert: '/my/pulpcore-postgres.crt',
+                pulpcore_postgresql_ssl_key: '/my/pulpcore-postgres.key',
+                pulpcore_postgresql_ssl_root_ca: '/my/root/ca.crt',
+              )
+            end
+
+            it { is_expected.to compile.with_all_deps }
+
+            it do
+              is_expected.to contain_class('pulpcore')
+                .with_postgresql_manage_db(false)
+                .with_postgresql_db_ssl(true)
+                .with_postgresql_db_ssl_require(true)
+                .with_postgresql_db_ssl_cert('/my/pulpcore-postgres.crt')
+                .with_postgresql_db_ssl_key('/my/pulpcore-postgres.key')
+                .with_postgresql_db_ssl_root_ca('/my/root/ca.crt')
+            end
+          end
         end
 
-        it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_class('foreman_proxy_content::pub_dir') }
-        it do
-          is_expected.to contain_class('pulpcore')
-            .with(apache_http_vhost: true)
-            .with(apache_https_vhost: true)
-            .that_comes_before('Class[foreman_proxy::plugin::pulp]')
-        end
-        it do
-          is_expected.to contain_class('foreman_proxy_content::reverse_proxy')
-            .with(path: '/')
-            .with(port: 8443)
-        end
-      end
+        context 'pulp-2to3-migration', if: facts[:operatingsystemmajrelease] == '7' do
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('pulpcore') }
 
-      context 'pulpcore with external postgres' do
-        let(:params) do
-          {
-            qpid_router: false,
-            pulpcore_manage_postgresql: false,
-            pulpcore_postgresql_host: 'postgres-pulpcore.example.com',
-            pulpcore_postgresql_port: 2345,
-            pulpcore_postgresql_user: 'pulpuser',
-            pulpcore_postgresql_password: 'sUpersEkret',
-            pulpcore_postgresql_db_name: 'pulpcore1'
-          }
-        end
-
-        let(:pre_condition) do
-          <<-PUPPET
-          include foreman_proxy
-          class { 'foreman_proxy::plugin::pulp':
-            enabled          => false,
-            pulpnode_enabled => false,
-            pulpcore_enabled => true,
-            pulpcore_mirror  => false,
-          }
-          PUPPET
-        end
-
-        it { is_expected.to compile.with_all_deps }
-
-        it do
-          is_expected.to contain_class('pulpcore')
-            .with_postgresql_manage_db(false)
-            .with_postgresql_db_host('postgres-pulpcore.example.com')
-            .with_postgresql_db_port(2345)
-            .with_postgresql_db_user('pulpuser')
-            .with_postgresql_db_password('sUpersEkret')
-            .with_postgresql_db_name('pulpcore1')
-            .with_postgresql_db_ssl(false)
-        end
-      end
-
-      context 'pulpcore with external postgres and SSL' do
-        let(:params) do
-          {
-            qpid_router: false,
-            pulpcore_manage_postgresql: false,
-            pulpcore_postgresql_ssl: true,
-            pulpcore_postgresql_ssl_require: true,
-            pulpcore_postgresql_ssl_cert: '/my/pulpcore-postgres.crt',
-            pulpcore_postgresql_ssl_key: '/my/pulpcore-postgres.key',
-            pulpcore_postgresql_ssl_root_ca: '/my/root/ca.crt',
-          }
-        end
-
-        let(:pre_condition) do
-          <<-PUPPET
-          include foreman_proxy
-          class { 'foreman_proxy::plugin::pulp':
-            enabled          => false,
-            pulpnode_enabled => false,
-            pulpcore_enabled => true,
-            pulpcore_mirror  => false,
-          }
-          PUPPET
-        end
-
-        it { is_expected.to compile.with_all_deps }
-
-        it do
-          is_expected.to contain_class('pulpcore')
-            .with_postgresql_manage_db(false)
-            .with_postgresql_db_ssl(true)
-            .with_postgresql_db_ssl_require(true)
-            .with_postgresql_db_ssl_cert('/my/pulpcore-postgres.crt')
-            .with_postgresql_db_ssl_key('/my/pulpcore-postgres.key')
-            .with_postgresql_db_ssl_root_ca('/my/root/ca.crt')
-        end
-      end
-
-      context 'pulp-2to3-migration', if: facts[:operatingsystemmajrelease] == '7' do
-        let(:params) do
-          {
-            qpid_router: false
-          }
-        end
-
-        let(:pre_condition) do
-          <<-PUPPET
-          include foreman_proxy
-          class { 'foreman_proxy::plugin::pulp':
-            enabled          => true,
-            pulpnode_enabled => false,
-            pulpcore_enabled => true,
-            pulpcore_mirror  => false,
-          }
-          PUPPET
-        end
-
-        it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_class('pulpcore') }
-
-        it do
-          is_expected.to contain_class('pulpcore::plugin::migration')
-            .with_mongo_db_name('pulp_database')
-            .with_mongo_db_seeds('localhost:27017')
-            .with_mongo_db_ssl(false)
-            .with_mongo_db_verify_ssl(true)
-            .with_mongo_db_ca_path('/etc/pki/tls/certs/ca-bundle.crt')
+          it do
+            is_expected.to contain_class('pulpcore::plugin::migration')
+              .with_mongo_db_name('pulp_database')
+              .with_mongo_db_seeds('localhost:27017')
+              .with_mongo_db_ssl(false)
+              .with_mongo_db_verify_ssl(true)
+              .with_mongo_db_ca_path('/etc/pki/tls/certs/ca-bundle.crt')
+          end
         end
       end
 
