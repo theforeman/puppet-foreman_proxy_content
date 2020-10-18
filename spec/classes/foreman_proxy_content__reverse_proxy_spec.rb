3ed90a6122c6caedc70ccf206eb8b6e375c60eec
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-10-18 15:09:45 +0200

    Set reverse proxy servername via certs
    The certs class already has the correct servername (set via init.pp) so
    this avoids a mismatch. It also adds aliases via the CNAME, which is new
    functionality.

--- spec/classes/foreman_proxy_content__reverse_proxy_spec.rb
+++ spec/classes/foreman_proxy_content__reverse_proxy_spec.rb
@@ -22,6 +22,7 @@ describe 'foreman_proxy_content::reverse_proxy' do
         it do
           is_expected.to contain_apache__vhost('katello-reverse-proxy')
             .with_servername(facts[:fqdn])
+            .with_aliases([])
             .with_port(8443)
             .with_proxy_pass([{
               'path' => '/',
@@ -33,22 +34,13 @@ describe 'foreman_proxy_content::reverse_proxy' do
       end
 
       describe 'with explicit parameters' do
-        let(:params) { { url: 'https://foreman.example.com/', servername: 'proxy.example.com', port: 443 } }
-
-        let(:pre_condition) do
-          <<-PUPPET
-          include foreman_proxy
-          class { 'foreman_proxy::plugin::pulp':
-            enabled          => false,
-            pulpnode_enabled => false,
-          }
-          PUPPET
-        end
+        let(:params) { { url: 'https://foreman.example.com/', port: 443 } }
 
         it { is_expected.to compile.with_all_deps }
         it do
           is_expected.to contain_apache__vhost('katello-reverse-proxy')
-            .with_servername('proxy.example.com')
+            .with_servername('foo.example.com')
+            .with_aliases([])
             .with_port(443)
             .without_keepalive # Not part of the vhost but used in the vhost_params
             .with_proxy_pass([{
@@ -59,6 +51,24 @@ describe 'foreman_proxy_content::reverse_proxy' do
             }])
           is_expected.to contain_concat__fragment('katello-reverse-proxy-proxy')
             .with_content(%r{^\s+ProxyPass / https://foreman\.example\.com/$})
+        end
+
+        describe 'with custom servername and cnames' do
+          let(:pre_condition) do
+            <<-PUPPET
+            class { 'certs::apache':
+              hostname => 'proxy.example.com',
+              cname    => ['proxy-01.example.com'],
+            }
+            PUPPET
+          end
+
+          it { is_expected.to compile.with_all_deps }
+          it do
+            is_expected.to contain_apache__vhost('katello-reverse-proxy')
+              .with_servername('proxy.example.com')
+              .with_aliases(['proxy-01.example.com'])
+          end
         end
 
         describe 'with vhost_params' do
