7a9fd5355d14178fa163caaf6c590035c27e6aad
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-02-06 00:31:30 +0100

    Group together spec tests
    Since b8a699ff9b3c2e3457c3165d33217d3657bee091 pulpcore is always
    enabled. That means `without parameters` and `with pulpcore` are the
    same.

--- spec/classes/foreman_proxy_content_spec.rb
+++ spec/classes/foreman_proxy_content_spec.rb
@@ -9,11 +9,6 @@ describe 'foreman_proxy_content' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_package('katello-debug') }
         it { is_expected.to contain_class('foreman_proxy_content::pub_dir') }
-      end
-
-      context 'with pulpcore' do
-        it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_class('foreman_proxy_content::pub_dir') }
         it do
           is_expected.to contain_class('pulpcore')
             .with(apache_http_vhost: 'foreman')
