198ca340f4d81f4aef88bb0fd38940d54958ba68
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-08-16 19:40:28 +0200

    Revert "Refs #32338 - Move content array param to params.pp"
    This reverts commit ba98d069944c31d07290d028b9f0ce722dc53592. The Kafo
    bug has been resolved and this workaround is no longer needed.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,8 +5,6 @@ class foreman_proxy_content::params {
 
   $qpid_router_sasl_password = extlib::cache_data('foreman_cache_data', 'qpid_router_sasl_password', extlib::random_password(16))
 
-  $pulpcore_allowed_content_checksums = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512']
-
   $pulpcore_postgresql_password   = extlib::cache_data('pulpcore_cache_data', 'db_password', extlib::random_password(32))
   $pulpcore_worker_count          = min(8, $facts['processors']['count'])
   $pulpcore_use_rq_tasking_system = false
