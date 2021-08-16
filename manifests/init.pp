theforeman/puppet-foreman_proxy_content

commit baa0e9ceae72f3cf14c354a2d1a05991ddd868a5

Refs #32891 - Optionally enable new Pulpcore tasking system
Requires https://github.com/theforeman/puppet-pulpcore/pull/203

manifests/init.pp
@@ -77,6 +77,10 @@
 #                                              degradation due to I/O blocking and is not recommended in most cases. Modifying this parameter should be done
 #                                              incrementally with benchmarking at each step to determine an optimal value for your deployment.
 #
+# $pulpcore_use_rq_tasking_system::            Use the older RQ workers tasking system instead of the newer PostgreSQL tasking system introduced in Pulpcore 3.13.
+#                                              Any benchmarking you did to optimize worker_count or other tasking related parameters will no longer be accurate after
+#                                              changing the tasking system. Do not modify this setting unless you understand the implications for performance and stability.
+#
 # $pulpcore_content_service_worker_timeout::   Gunicorn worker timeout in seconds for the pulpcore-content.service
 #
 # $pulpcore_api_service_worker_timeout::       Gunicorn worker timeout in seconds for the pulpcore-api.service
@@ -128,6 +132,7 @@ class foreman_proxy_content (
   Stdlib::Absolutepath $pulpcore_postgresql_ssl_key = '/etc/pki/katello/private/pulpcore-database.key',
   Stdlib::Absolutepath $pulpcore_postgresql_ssl_root_ca = '/etc/pki/tls/certs/ca-bundle.crt',
   Integer[0] $pulpcore_worker_count = $foreman_proxy_content::params::pulpcore_worker_count,
+  Boolean $pulpcore_use_rq_tasking_system = $foreman_proxy_content::params::pulpcore_use_rq_tasking_system,
   Optional[String[50]] $pulpcore_django_secret_key = undef,
   Integer[0] $pulpcore_content_service_worker_timeout = $foreman_proxy_content::params::pulpcore_content_service_worker_timeout,
   Integer[0] $pulpcore_api_service_worker_timeout = $foreman_proxy_content::params::pulpcore_api_service_worker_timeout,
@@ -262,6 +267,7 @@ class foreman_proxy_content (
     postgresql_db_ssl_key          => $pulpcore_postgresql_ssl_key,
     postgresql_db_ssl_root_ca      => $pulpcore_postgresql_ssl_root_ca,
     worker_count                   => $pulpcore_worker_count,
+    use_rq_tasking_system          => $pulpcore_use_rq_tasking_system,
     django_secret_key              => $pulpcore_django_secret_key,
     content_service_worker_timeout => $pulpcore_content_service_worker_timeout,
     api_service_worker_timeout     => $pulpcore_api_service_worker_timeout,

manifests/params.pp
@@ -7,8 +7,9 @@ class foreman_proxy_content::params {
 
   $pulpcore_allowed_content_checksums = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512']
 
-  $pulpcore_postgresql_password = extlib::cache_data('pulpcore_cache_data', 'db_password', extlib::random_password(32))
-  $pulpcore_worker_count        = min(8, $facts['processors']['count'])
+  $pulpcore_postgresql_password   = extlib::cache_data('pulpcore_cache_data', 'db_password', extlib::random_password(32))
+  $pulpcore_worker_count          = min(8, $facts['processors']['count'])
+  $pulpcore_use_rq_tasking_system = false
 
   $pulpcore_content_service_worker_timeout = 90
   $pulpcore_api_service_worker_timeout     = 90

commit 37a6e9eaa058fefd1788625ad5def7243cb351e5

Move more parameters from params.pp to init.pp
In baa0e9ceae72f3cf14c354a2d1a05991ddd868a5 another parameter was added
to params.pp, but booleans are fine to keep in init.pp. The same is true
for e291e2ba6bac2af4fab5697078ad48b4262eea5c.

Fixes: baa0e9ceae72f3cf14c354a2d1a05991ddd868a5
Fixes: e291e2ba6bac2af4fab5697078ad48b4262eea5c

manifests/init.pp
@@ -132,10 +132,10 @@ class foreman_proxy_content (
   Stdlib::Absolutepath $pulpcore_postgresql_ssl_key = '/etc/pki/katello/private/pulpcore-database.key',
   Stdlib::Absolutepath $pulpcore_postgresql_ssl_root_ca = '/etc/pki/tls/certs/ca-bundle.crt',
   Integer[0] $pulpcore_worker_count = $foreman_proxy_content::params::pulpcore_worker_count,
-  Boolean $pulpcore_use_rq_tasking_system = $foreman_proxy_content::params::pulpcore_use_rq_tasking_system,
+  Boolean $pulpcore_use_rq_tasking_system = false,
   Optional[String[50]] $pulpcore_django_secret_key = undef,
-  Integer[0] $pulpcore_content_service_worker_timeout = $foreman_proxy_content::params::pulpcore_content_service_worker_timeout,
-  Integer[0] $pulpcore_api_service_worker_timeout = $foreman_proxy_content::params::pulpcore_api_service_worker_timeout,
+  Integer[0] $pulpcore_content_service_worker_timeout = 90,
+  Integer[0] $pulpcore_api_service_worker_timeout = 90,
   Boolean $pulpcore_cache_enabled = false,
   Optional[Variant[Integer[1], Enum['None']]] $pulpcore_cache_expires_ttl = undef,
 ) inherits foreman_proxy_content::params {

manifests/params.pp
@@ -5,10 +5,6 @@ class foreman_proxy_content::params {
 
   $qpid_router_sasl_password = extlib::cache_data('foreman_cache_data', 'qpid_router_sasl_password', extlib::random_password(16))
 
-  $pulpcore_postgresql_password   = extlib::cache_data('pulpcore_cache_data', 'db_password', extlib::random_password(32))
-  $pulpcore_worker_count          = min(8, $facts['processors']['count'])
-  $pulpcore_use_rq_tasking_system = false
-
-  $pulpcore_content_service_worker_timeout = 90
-  $pulpcore_api_service_worker_timeout     = 90
+  $pulpcore_postgresql_password = extlib::cache_data('pulpcore_cache_data', 'db_password', extlib::random_password(32))
+  $pulpcore_worker_count = min(8, $facts['processors']['count'])
 }