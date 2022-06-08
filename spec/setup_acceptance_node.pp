0a10254d6761f798097cdf64a01c59d21a7d60ea
Eric D. Helms <ericdhelms@gmail.com>
2022-06-07 21:16:57 -0400

    Drop EL7 support

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -15,16 +15,10 @@ if $facts['os']['selinux']['enabled'] {
   }
 }
 
-if $facts['os']['release']['major'] == '8' {
-  package { 'glibc-langpack-en':
-    ensure => installed,
-  }
+package { 'glibc-langpack-en':
+  ensure => installed,
+}
 
-  yumrepo { 'powertools':
-    enabled => true,
-  }
-} elsif $facts['os']['release']['major'] == '7' {
-  package { 'epel-release':
-    ensure => installed,
-  }
+yumrepo { 'powertools':
+  enabled => true,
 }
