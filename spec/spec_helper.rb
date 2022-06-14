4c6dd311168497c4a012cd7bc19d1de3fc617136
Evgeni Golov <evgeni@golov.de>
2022-06-14 08:11:31 +0200

    explictly set service_provider fact in tests
    this comes from stdlib and is required in newer versions of the
    postgresql module
    
    (cherry picked from commit 42c53337ee0098a9507e9fc69e19b8babc4ddb5c)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'voxpupuli/test/spec_helper'
 
 add_custom_fact :systemd, true # puppet-systemd
 add_custom_fact :root_home, "/root" # puppetlabs-stdlib
+add_custom_fact :service_provider, "systemd" # puppetlabs-stdlib
 
 def get_content(subject, title)
   is_expected.to contain_file(title)
