theforeman/puppet-foreman_proxy_content

commit 2f9ada49f39b7cf821681d5cf9ce93fb0fa66f4d

Set reverse proxy servername via certs
The certs class already has the correct servername (set via init.pp) so
this avoids a mismatch. It also adds aliases via the CNAME, which is new
functionality.

manifests/reverse_proxy.pp
@@ -4,8 +4,6 @@
 #   The path where to mount the reverse proxy
 # @param url
 #   The URL to forward to
-# @param servername
-#   The Apache vhost server name to listen on
 # @param port
 #   The port to listen on
 # @param ssl_protocol
@@ -17,7 +15,6 @@
 class foreman_proxy_content::reverse_proxy (
   Stdlib::Unixpath $path = '/',
   Stdlib::Httpurl $url = "${foreman_proxy_content::foreman_url}/",
-  Stdlib::Fqdn $servername = $foreman_proxy_content::foreman_proxy_fqdn,
   Stdlib::Port $port = $foreman_proxy_content::reverse_proxy_port,
   Variant[Array[String], String, Undef] $ssl_protocol = undef,
   Hash[String, Any] $vhost_params = {},
@@ -30,7 +27,8 @@ class foreman_proxy_content::reverse_proxy (
   Class['certs', 'certs::ca', 'certs::apache', 'certs::foreman_proxy'] ~> Class['apache::service']
 
   apache::vhost { 'katello-reverse-proxy':
-    servername             => $servername,
+    servername             => $certs::apache::hostname,
+    aliases                => $certs::apache::cname,
     port                   => $port,
     docroot                => '/var/www/',
     priority               => '28',

spec/classes/foreman_proxy_content__reverse_proxy_spec.rb
@@ -22,6 +22,7 @@ describe 'foreman_proxy_content::reverse_proxy' do
         it do
           is_expected.to contain_apache__vhost('katello-reverse-proxy')
             .with_servername(facts[:fqdn])
+            .with_aliases([])
             .with_port(8443)
             .with_proxy_pass([{
               'path' => '/',
@@ -33,22 +34,13 @@ describe 'foreman_proxy_content::reverse_proxy' do
       end
 
       describe 'with explicit parameters' do
-        let(:params) { { url: 'https://foreman.example.com/', servername: 'proxy.example.com', port: 443 } }
-
-        let(:pre_condition) do
-          <<-PUPPET
-          include foreman_proxy
-          class { 'foreman_proxy::plugin::pulp':
-            enabled          => false,
-            pulpnode_enabled => false,
-          }
-          PUPPET
-        end
+        let(:params) { { url: 'https://foreman.example.com/', port: 443 } }
 
         it { is_expected.to compile.with_all_deps }
         it do
           is_expected.to contain_apache__vhost('katello-reverse-proxy')
-            .with_servername('proxy.example.com')
+            .with_servername('foo.example.com')
+            .with_aliases([])
             .with_port(443)
             .without_keepalive # Not part of the vhost but used in the vhost_params
             .with_proxy_pass([{
@@ -59,6 +51,24 @@ describe 'foreman_proxy_content::reverse_proxy' do
             }])
           is_expected.to contain_concat__fragment('katello-reverse-proxy-proxy')
             .with_content(%r{^\s+ProxyPass / https://foreman\.example\.com/$})
+        end
+
+        describe 'with custom servername and cnames' do
+          let(:pre_condition) do
+            <<-PUPPET
+            class { 'certs::apache':
+              hostname => 'proxy.example.com',
+              cname    => ['proxy-01.example.com'],
+            }
+            PUPPET
+          end
+
+          it { is_expected.to compile.with_all_deps }
+          it do
+            is_expected.to contain_apache__vhost('katello-reverse-proxy')
+              .with_servername('proxy.example.com')
+              .with_aliases(['proxy-01.example.com'])
+          end
         end
 
         describe 'with vhost_params' do

manifests/init.pp
@@ -219,7 +219,6 @@ class foreman_proxy_content (
     ~> class { 'foreman_proxy_content::reverse_proxy':
       path         => '/',
       url          => "${foreman_url}/",
-      servername   => $foreman_proxy_fqdn,
       port         => $reverse_proxy_port,
       subscribe    => Class['certs::foreman_proxy'],
       ssl_protocol => $ssl_protocol,

commit 1118a2b325274645918217574cfb50f10ef9b1cd

correctly set serveraliases for the reverse proxy
the correct parameter to `apache::vhost` is `serveraliases`, not
`aliases`. the later is for folder aliases and has totally different
syntax.

Fixes: 2f9ada49f39b7cf821681d5cf9ce93fb0fa66f4d

manifests/reverse_proxy.pp
@@ -37,7 +37,7 @@ define foreman_proxy_content::reverse_proxy (
   apache::vhost { $vhost_name:
     ensure                 => $ensure,
     servername             => $certs::apache::hostname,
-    aliases                => $certs::apache::cname,
+    serveraliases          => $certs::apache::cname,
     port                   => $port,
     docroot                => '/var/www/',
     priority               => $priority,

spec/defines/foreman_proxy_content__reverse_proxy_spec.rb
@@ -18,7 +18,7 @@ describe 'foreman_proxy_content::reverse_proxy' do
         it do
           is_expected.to contain_apache__vhost('my-reverse-proxy')
             .with_servername(facts[:fqdn])
-            .with_aliases([])
+            .with_serveraliases([])
             .with_port(8443)
             .with_proxy_pass([{
               'path' => '/',
@@ -37,7 +37,7 @@ describe 'foreman_proxy_content::reverse_proxy' do
         it do
           is_expected.to contain_apache__vhost('katello-reverse-proxy-443')
             .with_servername('foo.example.com')
-            .with_aliases([])
+            .with_serveraliases([])
             .with_port(443)
             .without_keepalive # Not part of the vhost but used in the vhost_params
             .with_proxy_pass([{
@@ -65,7 +65,7 @@ describe 'foreman_proxy_content::reverse_proxy' do
           it do
             is_expected.to contain_apache__vhost('katello-reverse-proxy')
               .with_servername('proxy.example.com')
-              .with_aliases(['proxy-01.example.com'])
+              .with_serveraliases(['proxy-01.example.com'])
           end
         end
 