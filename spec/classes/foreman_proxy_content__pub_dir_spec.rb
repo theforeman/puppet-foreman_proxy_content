cce53e902233a3d08899d8c77f0b156268affe67
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-07-15 11:54:43 +0200

    Fixes #35227 - Load apache::mod::autoindex if needed
    On content proxies we have (by default) /pub as a browsable directory,
    though it can be disabled by passing a different pub dir option. If it
    is browsable, mod_autoindex must be loaded. The installer disables
    loading default mods so it should be loaded explicitly.
    
    (cherry picked from commit ff90058acc0c45d5da6be196a742c06a1f2b96c3)

--- spec/classes/foreman_proxy_content__pub_dir_spec.rb
+++ spec/classes/foreman_proxy_content__pub_dir_spec.rb
@@ -8,7 +8,25 @@ describe 'foreman_proxy_content::pub_dir' do
       end
 
       it { should compile.with_all_deps }
+      it { should contain_class('apache::mod::autoindex') }
       it { should contain_package('katello-client-bootstrap') }
+
+      context 'apache without default mods and indexing disabled' do
+        let(:params) do
+          {
+            pub_dir_options: '+FollowSymLinks',
+          }
+        end
+        let(:pre_condition) do
+          <<~PUPPET
+          class { 'apache':
+            default_mods => false,
+          }
+          PUPPET
+        end
+
+        it { should_not contain_class('apache::mod::autoindex') }
+      end
     end
   end
 end
