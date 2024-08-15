theforeman/puppet-foreman_proxy_content

commit 76e2a6852d1d2ca33935ccf8a6ab69992c32ec1d

Fixes #33863 - Allow registration through content proxy via port 443

manifests/init.pp
@@ -156,10 +156,7 @@ class foreman_proxy_content (
   $shared_with_foreman_vhost = !$pulpcore_mirror
 
   $rhsm_path = '/rhsm'
-  $rhsm_port = $reverse_proxy_real ? {
-    true  => $reverse_proxy_port,
-    false => 443
-  }
+  $rhsm_port = 443
 
   ensure_packages('katello-debug')
 
@@ -167,9 +164,25 @@ class foreman_proxy_content (
   Class['certs::foreman_proxy'] ~> Service['foreman-proxy']
 
   if $reverse_proxy_real {
-    class { 'foreman_proxy_content::reverse_proxy':
-      url  => "${foreman_url}/",
-      port => $reverse_proxy_port,
+    foreman_proxy_content::reverse_proxy { "rhsm-pulpcore-https-${reverse_proxy_port}":
+      url      => "${foreman_url}/",
+      port     => $reverse_proxy_port,
+      priority => '10',
+      before   => Class['pulpcore::apache'],
+    }
+  }
+
+  if $pulpcore_mirror {
+    $pulpcore_https_vhost_name = "rhsm-pulpcore-https-${rhsm_port}"
+
+    if $rhsm_port != $reverse_proxy_port {
+      foreman_proxy_content::reverse_proxy { $pulpcore_https_vhost_name:
+        path     => $rhsm_path,
+        url      => "${foreman_url}${rhsm_path}",
+        port     => $rhsm_port,
+        priority => '10',
+        before   => Class['pulpcore::apache'],
+      }
     }
   }
 
@@ -232,13 +245,13 @@ class foreman_proxy_content (
     include certs::apache
     Class['certs::apache'] ~> Class['pulpcore::apache']
     $servername = $certs::apache::hostname
-    $priority = undef
+    $priority = '10'
     $apache_http_vhost = undef
-    $apache_https_vhost = undef
-    $apache_https_cert = $certs::apache::apache_cert
-    $apache_https_key = $certs::apache::apache_key
-    $apache_https_ca = $certs::katello_default_ca_cert
-    $apache_https_chain = $certs::katello_server_ca_cert
+    $apache_https_vhost = $pulpcore_https_vhost_name
+    $apache_https_cert = undef
+    $apache_https_key = undef
+    $apache_https_ca = undef
+    $apache_https_chain = undef
   }
 
   $api_client_auth_cn_map = Hash($foreman_proxy::trusted_hosts.map |$host| {
@@ -283,7 +296,10 @@ class foreman_proxy_content (
   if $enable_docker {
     include pulpcore::plugin::container
     unless $shared_with_foreman_vhost {
-      include foreman_proxy_content::container
+      class { 'foreman_proxy_content::container':
+        pulpcore_https_vhost => $apache_https_vhost,
+      }
+
       class { 'foreman_proxy::plugin::container_gateway':
         pulp_endpoint => "https://${servername}",
       }

manifests/reverse_proxy.pp
@@ -12,13 +12,19 @@
 #   Any parameters to pass to the apache::vhost resource
 # @param proxy_pass_params
 #   Any parameters to pass to the proxy_pass param of the apache::vhost resource
-class foreman_proxy_content::reverse_proxy (
+# @param ensure
+#   Specifies if the virtual host is present or absent.
+# @param priority
+#   Sets the relative load-order for Apache HTTPD VirtualHost configuration files. See Apache::Vhost
+define foreman_proxy_content::reverse_proxy (
   Stdlib::Unixpath $path = '/',
   Stdlib::Httpurl $url = "${foreman_proxy_content::foreman_url}/",
   Stdlib::Port $port = $foreman_proxy_content::reverse_proxy_port,
   Variant[Array[String], String, Undef] $ssl_protocol = undef,
   Hash[String, Any] $vhost_params = {},
   Hash[String, Variant[String, Integer]] $proxy_pass_params = {'disablereuse' => 'on', 'retry' => '0'},
+  Enum['present', 'absent'] $ensure = 'present',
+  Optional[Variant[String, Boolean]] $priority = '28',
 ) {
   include apache
   include certs::apache
@@ -26,12 +32,15 @@ class foreman_proxy_content::reverse_proxy (
 
   Class['certs', 'certs::ca', 'certs::apache', 'certs::foreman_proxy'] ~> Class['apache::service']
 
-  apache::vhost { 'katello-reverse-proxy':
+  $vhost_name = $title
+
+  apache::vhost { $vhost_name:
+    ensure                 => $ensure,
     servername             => $certs::apache::hostname,
     aliases                => $certs::apache::cname,
     port                   => $port,
     docroot                => '/var/www/',
-    priority               => '28',
+    priority               => $priority,
     ssl_options            => ['+StdEnvVars', '+ExportCertData', '+FakeBasicAuth'],
     ssl                    => true,
     ssl_proxyengine        => true,

manifests/container.pp
@@ -8,10 +8,13 @@
 # @param registry_v2_path
 #   The path beneath the location prefix to forward. This is also appended to
 #   the content base url.
+# @param pulpcore_https_vhost
+#   The name of the Apache https vhost for Pulpcore
 class foreman_proxy_content::container (
   String $location_prefix = '/pulpcore_registry',
   String $registry_v1_path = '/v1/',
   String $registry_v2_path = '/v2/',
+  String $pulpcore_https_vhost = 'pulpcore-https',
 ) {
   include certs::foreman_proxy
 
@@ -39,7 +42,7 @@ class foreman_proxy_content::container (
   $https_content = epp('foreman_proxy_content/container-gateway-fragment.epp', $context)
 
   apache::vhost::fragment { 'pulp-https-container':
-    vhost    => 'pulpcore-https',
+    vhost    => $pulpcore_https_vhost,
     priority => '10',
     content  => $https_content,
   }

spec/classes/foreman_proxy_content__container_spec.rb
@@ -29,12 +29,12 @@ describe 'foreman_proxy_content::container' do
       end
 
       describe 'with explicit parameters' do
-        let(:params) { { location_prefix: '/other_pulpcore_registry', registry_v1_path: '/vr1/', registry_v2_path: '/vr2/' } }
+        let(:params) { { location_prefix: '/other_pulpcore_registry', registry_v1_path: '/vr1/', registry_v2_path: '/vr2/', pulpcore_https_vhost: 'rhsm-pulpcore-reverse-proxy-443' } }
 
         it { is_expected.to compile.with_all_deps }
         it do
           is_expected.to contain_apache__vhost__fragment('pulp-https-container')
-            .with_vhost('pulpcore-https')
+            .with_vhost('rhsm-pulpcore-reverse-proxy-443')
             .with_priority('10')
             .with_content(%r{^\s+<Location "/other_pulpcore_registry/vr2/">$})
             .with_content(%r{^\s+Require expr %\{SSL_CLIENT_S_DN_CN\} == "foo.example.com"$})

spec/classes/foreman_proxy_content_spec.rb
@@ -133,17 +133,26 @@ describe 'foreman_proxy_content' do
         it do
           is_expected.to contain_class('pulpcore')
             .with(apache_http_vhost: true)
-            .with(apache_https_vhost: true)
+            .with(apache_https_vhost: 'rhsm-pulpcore-https-443')
             .that_comes_before('Class[foreman_proxy::plugin::pulp]')
         end
         it do
           is_expected.to contain_class('foreman_proxy::plugin::pulp')
-            .with_rhsm_url("https://#{facts[:fqdn]}:8443/rhsm")
+            .with_rhsm_url("https://#{facts[:fqdn]}:443/rhsm")
         end
         it do
-          is_expected.to contain_class('foreman_proxy_content::reverse_proxy')
+          is_expected.to contain_foreman_proxy_content__reverse_proxy('rhsm-pulpcore-https-8443')
             .with(path: '/')
             .with(port: 8443)
+            .with(priority: '10')
+            .that_comes_before('Class[pulpcore::apache]')
+        end
+        it do
+          is_expected.to contain_foreman_proxy_content__reverse_proxy('rhsm-pulpcore-https-443')
+            .with(path: '/rhsm')
+            .with(port: 443)
+            .with(priority: '10')
+            .that_comes_before('Class[pulpcore::apache]')
         end
         it do
           is_expected.to contain_pulpcore__apache__fragment('gpg_key_proxy')

spec/defines/foreman_proxy_content__reverse_proxy_spec.rb
@@ -4,8 +4,9 @@ describe 'foreman_proxy_content::reverse_proxy' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
+      let(:title) { 'my-reverse-proxy' }
 
-      describe 'with inherited parameters' do
+      context 'with inherited parameters' do
 
         let(:pre_condition) do
           <<-PUPPET
@@ -15,7 +16,7 @@ describe 'foreman_proxy_content::reverse_proxy' do
 
         it { is_expected.to compile.with_all_deps }
         it do
-          is_expected.to contain_apache__vhost('katello-reverse-proxy')
+          is_expected.to contain_apache__vhost('my-reverse-proxy')
             .with_servername(facts[:fqdn])
             .with_aliases([])
             .with_port(8443)
@@ -28,12 +29,13 @@ describe 'foreman_proxy_content::reverse_proxy' do
         end
       end
 
-      describe 'with explicit parameters' do
+      context 'with explicit parameters' do
         let(:params) { { url: 'https://foreman.example.com/', port: 443 } }
+        let(:title) { 'katello-reverse-proxy-443' }
 
         it { is_expected.to compile.with_all_deps }
         it do
-          is_expected.to contain_apache__vhost('katello-reverse-proxy')
+          is_expected.to contain_apache__vhost('katello-reverse-proxy-443')
             .with_servername('foo.example.com')
             .with_aliases([])
             .with_port(443)
@@ -44,11 +46,11 @@ describe 'foreman_proxy_content::reverse_proxy' do
               'reverse_urls' => ['https://foreman.example.com/'],
               'params' => {'disablereuse' => 'on', 'retry' => '0'},
             }])
-          is_expected.to contain_concat__fragment('katello-reverse-proxy-proxy')
+          is_expected.to contain_concat__fragment('katello-reverse-proxy-443-proxy')
             .with_content(%r{^\s+ProxyPass / https://foreman\.example\.com/ disablereuse=on retry=0$})
         end
 
-        describe 'with custom servername and cnames' do
+        context 'with custom servername and cnames' do
           let(:pre_condition) do
             <<-PUPPET
             class { 'certs::apache':
@@ -57,6 +59,7 @@ describe 'foreman_proxy_content::reverse_proxy' do
             }
             PUPPET
           end
+          let(:title) { 'katello-reverse-proxy' }
 
           it { is_expected.to compile.with_all_deps }
           it do
@@ -66,15 +69,17 @@ describe 'foreman_proxy_content::reverse_proxy' do
           end
         end
 
-        describe 'with vhost_params' do
+        context 'with vhost_params' do
           let(:params) { super().merge(vhost_params: {keepalive: 'on'}) }
+          let(:title) { 'katello-reverse-proxy' }
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_apache__vhost('katello-reverse-proxy').with_keepalive('on') }
         end
 
-        describe 'with proxy_pass_params' do
+        context 'with proxy_pass_params' do
           let(:params) { super().merge(proxy_pass_params: {disablereuse: 'off'}) }
+          let(:title) { 'katello-reverse-proxy' }
 
           it { is_expected.to compile.with_all_deps }
           it do

commit 950cdf489283261c7a885fab5322bfc0aadde6f2

Fixes #37620 - Use the Pulpcore docroot for reverse proxy
In 804e193b6a180e00ac2828b33a2c11d9a587e5cb /var/www was used as a
docroot, but since / was proxied to Foreman this wasn't an issue. Since
76e2a6852d1d2ca33935ccf8a6ab69992c32ec1d that is no longer the case and
it's now listed.

Pulpcore's docroot is a known safe directory without irrelevant files
that shouldn't be hosted and correct SELinux labels.

It also disables indexes and multiviews on the directory because some
stupid "security" scanners trip on it. This should be safe because the
directory is not used elsewhere. That way we keep listing as an option
for /pub.

Fixes: 76e2a6852d1d2ca33935ccf8a6ab69992c32ec1d

manifests/init.pp
@@ -146,22 +146,6 @@ class foreman_proxy_content (
     }
   }
 
-  if $pulpcore_mirror {
-    $pulpcore_https_vhost_name = "rhsm-pulpcore-https-${rhsm_port}"
-
-    if $rhsm_port != $reverse_proxy_port {
-      foreman_proxy_content::reverse_proxy { $pulpcore_https_vhost_name:
-        path_url_map => {
-          $rhsm_path     => "${proxy_foreman_url}${rhsm_path}",
-          $insights_path => "${proxy_foreman_url}${insights_path}",
-        },
-        port         => $rhsm_port,
-        priority     => '10',
-        before       => Class['pulpcore::apache'],
-      }
-    }
-  }
-
   include foreman_proxy_content::pub_dir
 
   if $pulpcore_mirror {
@@ -199,7 +183,7 @@ class foreman_proxy_content (
     $serveraliases = $certs::apache::cname
     $priority = '10'
     $apache_http_vhost = undef
-    $apache_https_vhost = $pulpcore_https_vhost_name
+    $apache_https_vhost = "rhsm-pulpcore-https-${rhsm_port}"
     $apache_https_cert = undef
     $apache_https_key = undef
     $apache_https_ca = undef
@@ -266,6 +250,19 @@ class foreman_proxy_content (
       cert         => $certs::foreman::client_cert,
       key          => $certs::foreman::client_key,
       require      => Class['certs::foreman'],
+    }
+  } elsif $pulpcore_mirror {
+    if $rhsm_port != $reverse_proxy_port {
+      foreman_proxy_content::reverse_proxy { $apache_https_vhost:
+        docroot      => $pulpcore::apache_docroot,
+        path_url_map => {
+          $rhsm_path     => "${proxy_foreman_url}${rhsm_path}",
+          $insights_path => "${proxy_foreman_url}${insights_path}",
+        },
+        port         => $rhsm_port,
+        priority     => '10',
+        before       => Class['pulpcore::apache'],
+      }
     }
   }
 

manifests/reverse_proxy.pp
@@ -1,5 +1,7 @@
 # Adds http reverse-proxy to parent conf
 #
+# @param docroot
+#   The document root for the reverse proxy to use
 # @param path_url_map
 #   The paths and corresponding URLs where to mount the reverse proxy
 # @param port
@@ -15,6 +17,7 @@
 # @param priority
 #   Sets the relative load-order for Apache HTTPD VirtualHost configuration files. See Apache::Vhost
 define foreman_proxy_content::reverse_proxy (
+  Stdlib::Absolutepath $docroot = '/var/www/html',
   Hash[Stdlib::Unixpath, String[1]] $path_url_map = { '/' => "${foreman_proxy_content::foreman_url}/" },
   Stdlib::Port $port = $foreman_proxy_content::reverse_proxy_port,
   Variant[Array[String], String, Undef] $ssl_protocol = undef,
@@ -45,8 +48,9 @@ define foreman_proxy_content::reverse_proxy (
     servername             => $certs::apache::hostname,
     serveraliases          => $certs::apache::cname,
     port                   => $port,
-    docroot                => '/var/www/',
+    docroot                => $docroot,
     priority               => $priority,
+    options                => ['FollowSymLinks'],
     ssl_options            => ['+StdEnvVars', '+ExportCertData', '+FakeBasicAuth'],
     ssl                    => true,
     ssl_proxyengine        => true,

spec/acceptance/content_standalone_mirror_spec.rb
@@ -35,8 +35,7 @@ describe 'pulpcore mirror' do
 
     describe file('/etc/httpd/conf.d/10-rhsm-pulpcore-https-443.conf') do
       it { is_expected.to be_file }
-      # TODO: this is a regression introduced in 76e2a6852d1d2ca33935ccf8a6ab69992c32ec1d
-      it { is_expected.to contain(%{DocumentRoot "/var/www}) }
+      it { is_expected.to contain('DocumentRoot "/var/lib/pulp/pulpcore_static"') }
     end
 
     describe port('443') do
@@ -83,8 +82,7 @@ describe 'pulpcore mirror' do
 
     describe file('/etc/httpd/conf.d/10-rhsm-pulpcore-https-443.conf') do
       it { is_expected.to be_file }
-      # TODO: this is a regression introduced in 76e2a6852d1d2ca33935ccf8a6ab69992c32ec1d
-      it { is_expected.to contain(%{DocumentRoot "/var/www}) }
+      it { is_expected.to contain('DocumentRoot "/var/lib/pulp/pulpcore_static"') }
     end
 
     describe port('443') do